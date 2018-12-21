8571bc4007fa07cde47e5c6a091457f6e4a357ca
cosmin <cosmin@bcnp.ro>
2018-12-21 18:29:57 +0200

    some rainy fall and also a little experiments

--- 2.js
+++ 2.js
@@ -1,4 +1,4 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{27:function(e,t,n){var r;
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{25:function(e,t,n){var r;
 /*!
  * jQuery JavaScript Library v3.3.1
  * https://jquery.com/
