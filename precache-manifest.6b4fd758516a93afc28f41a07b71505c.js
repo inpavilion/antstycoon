97729cadf0cb50f0893cfad3cfefa00d2e75ec47
cosmin <cosmin@bcnp.ro>
2019-02-15 01:25:07 +0200

    some changes

---
+++ precache-manifest.6b4fd758516a93afc28f41a07b71505c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "06a44ff7febb2025216a493d61416263",
+    "url": "/static/media/logo.06a44ff7.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f0050a4cc19c01e60eb4",
+    "url": "/static/js/main.f0050a4c.chunk.js"
+  },
+  {
+    "revision": "0f99de15d4a388f669d8",
+    "url": "/static/js/1.0f99de15.chunk.js"
+  },
+  {
+    "revision": "f0050a4cc19c01e60eb4",
+    "url": "/static/css/main.55d8c5b1.chunk.css"
+  },
+  {
+    "revision": "0f99de15d4a388f669d8",
+    "url": "/static/css/1.4b486b92.chunk.css"
+  },
+  {
+    "revision": "529ebde43931cb24cf87a3a9f7fa3f6d",
+    "url": "/index.html"
+  }
+];
