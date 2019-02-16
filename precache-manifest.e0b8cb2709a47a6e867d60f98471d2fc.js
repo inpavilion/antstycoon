271abf5fc66cfaf3a3fb8e50e67aa7af1031898d
cosmin <cosmin@bcnp.ro>
2019-02-16 20:37:51 +0200

    salutr

---
+++ precache-manifest.e0b8cb2709a47a6e867d60f98471d2fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "06a44ff7febb2025216a493d61416263",
+    "url": "/antstycoon/static/media/logo.06a44ff7.svg"
+  },
+  {
+    "revision": "d9f6f2258314b8282451",
+    "url": "/antstycoon/static/js/runtime~main.d9f6f225.js"
+  },
+  {
+    "revision": "aa4ace8ad6f11b68068d",
+    "url": "/antstycoon/static/js/main.aa4ace8a.chunk.js"
+  },
+  {
+    "revision": "e47fb7b952f0f3b03255",
+    "url": "/antstycoon/static/js/1.e47fb7b9.chunk.js"
+  },
+  {
+    "revision": "aa4ace8ad6f11b68068d",
+    "url": "/antstycoon/static/css/main.feba4814.chunk.css"
+  },
+  {
+    "revision": "e47fb7b952f0f3b03255",
+    "url": "/antstycoon/static/css/1.26117106.chunk.css"
+  },
+  {
+    "revision": "fa2deadd3c7e58a780051568f5346d2b",
+    "url": "/antstycoon/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/antstycoon/precache-manifest.328b9de530ffc70babdb7c07766ab96c.js"
+  "/antstycoon/precache-manifest.e0b8cb2709a47a6e867d60f98471d2fc.js"
 );
 
 workbox.clientsClaim();
